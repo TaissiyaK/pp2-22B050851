f9af9af6f21095ad2a403c7c871067167082377c
Taissiya <kuroedova.t@gmail.com>
2023-01-23 21:52:01 +0300

    9

--- TSIS1/strings.py
+++ TSIS1/strings.py
@@ -38,3 +38,82 @@ print("expensive" not in txt)
 txt = "The best things in life are free!"
 if "expensive" not in txt:
   print("No, 'expensive' is NOT present.")
+
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+b = "Hello, World!"
+print(b[2:])
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+
+txt = "We are the so-called \"Vikings\" from the north."
+
+#\'	Single Quote	
+#\\	Backslash	
+#\n	New Line	
+#\r	Carriage Return	
+#\t	Tab	
+#\b	Backspace	
+#\f	Form Feed	
+#\ooo	Octal value	
+#\xhh	Hex value
+
+#capitalize()	Converts the first character to upper case
+#casefold()	Converts string into lower case
+#center()	Returns a centered string
+#count()	Returns the number of times a specified value occurs in a string
+#encode()	Returns an encoded version of the string
+#endswith()	Returns true if the string ends with the specified value
+#expandtabs()	Sets the tab size of the string
+#find()	Searches the string for a specified value and returns the position of where it was found
+#format()	Formats specified values in a string
+#format_map()	Formats specified values in a string
+#index()	Searches the string for a specified value and returns the position of where it was found
+#isalnum()	Returns True if all characters in the string are alphanumeric
+#isalpha()	Returns True if all characters in the string are in the alphabet
