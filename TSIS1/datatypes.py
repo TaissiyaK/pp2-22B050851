93097181a9e7f992084a83c465cb61e38d020b76
Taissiya <kuroedova.t@gmail.com>
2023-01-23 21:14:43 +0300

    6

---
+++ TSIS1/datatypes.py
@@ -0,0 +1,62 @@
+x = 5
+print(type(x))
+
+x = "Hello World"
+print(type(x))
+x = 20
+print(type(x))
+x = 20.5
+print(type(x))
+x = 1j
+print(type(x))
+x = ["apple", "banana", "cherry"]
+print(type(x))	
+x = ("apple", "banana", "cherry")
+print(type(x))
+x = range(6)	
+print(type(x))
+x = {"name" : "John", "age" : 36}
+print(type(x))
+x = {"apple", "banana", "cherry"}
+print(type(x))
+x = frozenset({"apple", "banana", "cherry"})
+print(type(x))
+x = True
+print(type(x))
+x = b"Hello"
+print(type(x))
+x = bytearray(5)
+print(type(x))
+x = memoryview(bytes(5))
+print(type(x))	
+x = None
+print(type(x))
+
+x = str("Hello World")	#str	
+print(type(x))
+x = int(20)	#int
+print(type(x))	
+x = float(20.5)	#float
+print(type(x))	
+x = complex(1j)	#complex	
+print(type(x))
+x = list(("apple", "banana", "cherry"))	#list
+print(type(x))	
+x = tuple(("apple", "banana", "cherry"))	#tuple
+print(type(x))	
+x = range(6)	#range
+print(type(x))	
+x = dict(name="John", age=36)	#dict
+print(type(x))	
+x = set(("apple", "banana", "cherry"))	#set	
+print(type(x))
+x = frozenset(("apple", "banana", "cherry"))	#frozenset	
+print(type(x))
+x = bool(5)	#bool	
+print(type(x))
+x = bytes(5)	#bytes
+print(type(x))	
+x = bytearray(5)	#bytearray
+print(type(x))	
+x = memoryview(bytes(5))
+print(type(x))
