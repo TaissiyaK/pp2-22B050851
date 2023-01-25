b1d04cd82986cb2c0b48a9b730588925bf0b377a
Taissiya <kuroedova.t@gmail.com>
2023-01-25 14:05:23 +0300

    access list items

--- TSIS2/lists.py
+++ TSIS2/lists.py
@@ -19,3 +19,27 @@ print(type(mylist))
 thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
 print(thislist)
 
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+
+
+
