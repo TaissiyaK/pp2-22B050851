f9ad8eb2042a265abc85799accda89a7eeed2d15
Taissiya <kuroedova.t@gmail.com>
2023-02-21 09:37:01 +0300

    new generators

--- TSIS4/generators/3.py
+++ TSIS4/generators/3.py
@@ -1,9 +1,8 @@
-def div(numbers):
-    for i in numbers:
+def div(n):
+    for i in range(n+1):
         if i%3 == 0 and i%4 == 0:
             yield i
 
 n = int(input())
-numbers = (i for i in range(n+1))
-for i in div(numbers):
+for i in div(n):
     print(i)
--- TSIS4/generators/4.py
+++ TSIS4/generators/4.py
@@ -1,9 +1,8 @@
-def squares(numbers):
-    for i in numbers:
+def squares(a,b):
+    for i in range(a,b+1):
         yield i*i
 
 a, b = int(input()), int(input())
-numbers = (i for i in range(a,b+1))
-for i in squares(numbers):
+for i in squares(a,b):
     print(i)
 
